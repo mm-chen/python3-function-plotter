18e0f8dac53b0967c50a72a9f7670fda8d9d3562
mm-chen <github@malte-modler.de>
2020-04-23 12:56:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 #!/usr/bin/python3
 import math;
+import sys;
 
-x=-10
-c=-1.25
-while c<1.25:
-	while x<=10:
-		if(x==0 and c==0):
-			print("0",end="");
-		elif (x%10==0 or c%10==0):
-			print("+",end="");
-		elif(x==0):
-			print("|",end="");
-		elif(c==0):
-			print("-",end="");
-		elif(((math.sin(0.5*x) + c)/2+(math.sin(2*x) + c)/2)>0):
-			print("#",end="");
-		#elif((math.sin(2*x) + c)>0):
-		#	print("o",end="");
+startX=-11.0
+stopX=11.0
+interX=0.1
+startY=-11.0
+stopY=11.0
+interY=0.1
+
+marks=1.0
+
+x=startX
+y=startY
+while y<stopY:
+	while x<=stopX:
+		if  ((interX/2>x>-interX/2) and (interY/2>y>-interY/2)):
+			sys.stdout.write("0");
+		elif((interX/2>abs(x)%marks>-interX/2 or interX/2>marks-abs(x)%marks>-interX/2) and (interY/2>abs(y)%marks>-interY/2 or interY/2>marks-abs(y)%marks>-interY/2)):
+			sys.stdout.write("+");
+		elif(interX/2>abs(x)%marks>-interX/2 or interX/2>marks-abs(x)%marks>-interX/2):
+			sys.stdout.write("|");
+		elif(interY/2>abs(y)%marks>-interY/2 or interY/2>marks-abs(y)%marks>-interY/2):
+			sys.stdout.write("-",);
+		elif((math.sqrt(abs(x)) + y)>0):
+			sys.stdout.write("\033[31m#\033[0m");
+		elif(((x*x) + y)>0):
+			sys.stdout.write("\033[32mo\033[0m");
 		else:
-			print("-",end="");
-		#print ("x =", x, "\n\t\t y =", int(f(x)));
-		x+=0.125
-	c+=0.0625
-	x=-10
-	print();
+			sys.stdout.write(" ");
+		x+=interX
+	y+=interY
+	x=startX
+	sys.stdout.write("\n");
 
 quit(0);
